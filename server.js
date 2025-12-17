e5f1bdd78e5fad01b5a9123ed3a4d12e9895416e
Mizu Ahmed <raihanmizu70@gmail.com>
2025-12-17 17:00:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,19 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/submit", require("./routes/submit"));
 app.use("/api/withdraw", require("./routes/withdraw"));
 app.use("/api/admin", require("./routes/admin"));
 
 app.get("/", (req, res) => {
-  res.send("DVT Backend Running 🚀");
+  res.send("DVT Static Backend Running ✅");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on", PORT);
 });
